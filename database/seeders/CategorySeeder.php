61bc38c1a1d51af202640af5c40cac05fa4f3d20
Arundaya Pratama Nurhasan <120204570+Sirund@users.noreply.github.com>
2024-12-25 07:52:33 +0700

    ~

---
+++ database/seeders/CategorySeeder.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Category;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class CategorySeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        // Category::factory(3)->create();
+
+        Category::create([
+            'name' => 'Web Design',
+            'slug' => 'web-design',
+            'color' => 'red'
+        ]);
+        Category::create([
+            'name' => 'UI UX',
+            'slug' => 'ui-ux',
+            'color' => 'green'
+        ]);
+        Category::create([
+            'name' => 'Machine Learnig',
+            'slug' => 'machine-learning',
+            'color' => 'blue'
+        ]);
+        Category::create([
+            'name' => 'Data structure',
+            'slug' => 'data-structure',
+            'color' => 'yellow'
+        ]);
+        Category::create([
+            'name' => 'Cyber Security',
+            'slug' => 'cyber-security',
+            'color' => 'purple'
+        ]);
+    }
+}
